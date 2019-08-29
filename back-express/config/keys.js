c33f9e33e1293101db897e0ada81d59b238ea0dc
Dullboijake <1262283788@qq.com>
2019-08-30 01:51:51 +0800

    divide back end

--- back-express/config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    MongoURL: 'mongodb+srv://Tokunaga:tokunaga24253@image-host-sf1ux.mongodb.net/test?retryWrites=true&w=majority'
-}
--- back-express/middleware/logger.js
+++
@@ -1,8 +0,0 @@
-let time = new Date();
-
-const logger = (req,res,next) => {
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}:${time.toLocaleDateString()}`);
-    next();
-};
-
-module.exports = logger;
